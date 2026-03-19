c37e5d168e65d74285d77b59ed908edf8a13e0c4
piyu820 <priyankasadaye07@gmail.com>
2026-03-19 20:59:11 +0530

    openai setup

--- logic/ai_chatbot.py
+++ logic/ai_chatbot.py
@@ -1,6 +1,7 @@
+import os
 from openai import OpenAI
 
-client = OpenAI(api_key="YOUR_API_KEY")
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 
 def generate_interview_response(resume_text, user_answer, history):
